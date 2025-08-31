30274b145b48a706c52203dd2fcbf83495fe0f4d
arellaTV <jeremiah.arella@gmail.com>
2025-08-30 23:52:00 -0700

    Initializes project

---
+++ README.md
@@ -0,0 +1,8 @@
+<img src="gfx/top_screen.png" />
+<img src="gfx/bottom_screen.png" />
+
+# Quizzler 3Ds
+
+This is my first 3Ds homebrew game. The questions are sourced from [Open Trivia DB](https://opentdb.com/) created by PIXELTAIL Games LLC (License: [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/)).
+
+Special thanks to all of the developers who have contributed to [devkitPro](https://devkitpro.org/) and the examples found in [3ds-examples](https://github.com/devkitPro/3ds-examples).
---
+++ source/main.c
@@ -0,0 +1,78 @@
+#include <citro2d.h>
+
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct
+{
+	C2D_Sprite spr;
+	float dx, dy; // velocity
+} Sprite;
+
+static C2D_SpriteSheet spriteSheet;
+static Sprite sprites[1];
+
+//---------------------------------------------------------------------------------
+static void initSprites() {
+//---------------------------------------------------------------------------------
+  Sprite* sprite = &sprites[0];
+  C2D_SpriteFromSheet(&sprite->spr, spriteSheet, 0);
+  C2D_SpriteSetPos(&sprite->spr, 0, 0);
+}
+
+//---------------------------------------------------------------------------------
+int main(int argc, char* argv[]) {
+//---------------------------------------------------------------------------------
+	// Init libs
+	romfsInit();
+	gfxInitDefault();
+	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+	C2D_Prepare();
+	consoleInit(GFX_BOTTOM, NULL);
+
+	// Create screens
+	C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
+
+	// Load graphics
+	spriteSheet = C2D_SpriteSheetLoad("romfs:/gfx/sprites.t3x");
+	if (!spriteSheet) svcBreak(USERBREAK_PANIC);
+
+	// Initialize sprites
+	initSprites();
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		hidScanInput();
+
+		// Respond to user input
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
+
+		printf("\x1b[2;1HCPU:     %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f);
+		printf("\x1b[3;1HGPU:     %6.2f%%\x1b[K", C3D_GetDrawingTime()*6.0f);
+		printf("\x1b[4;1HCmdBuf:  %6.2f%%\x1b[K", C3D_GetCmdBufUsage()*100.0f);
+
+		// Render the scene
+		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		C2D_TargetClear(top, C2D_Color32f(0.0f, 0.0f, 0.0f, 1.0f));
+		C2D_SceneBegin(top);
+    C2D_DrawSprite(&sprites[0].spr);			
+		C3D_FrameEnd(0);
+	}
+
+	// Delete graphics
+	C2D_SpriteSheetFree(spriteSheet);
+
+	// Deinit libs
+	C2D_Fini();
+	C3D_Fini();
+	gfxExit();
+	romfsExit();
+	return 0;
+}
